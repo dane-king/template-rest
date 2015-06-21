8e750b10560df348ff602aa042392e312d26c96f
dane-king <kingd9@gmail.com>
2015-06-21 05:59:24 -0400

    more refactorings

--- collection-json/client.js
+++ collection-json/client.js
@@ -6,48 +6,42 @@ var item_template;
 var query_template;
 var template_template;
 
-function isId(element, index, array) {
+function isId(element) {
     if(element.name==='id'){
         return true
     }
 };
 
-$(document).ready(function() {
-    Handlebars.registerPartial('link','<a class="{{rel}}" href="{{href}}{{>query_param}}">{{prompt}}</a>');
-    Handlebars.registerPartial('query_param','{{#each data}}{{#if @first}}?{{/if}}{{name}}={{value}}{{#unless @last}}&{{/unless}}{{/each}}');
-    Handlebars.registerPartial('input','<label for="{{name}}">{{prompt}}</label><input name="{{name}}" value="{{value}}"/>');
-    Handlebars.registerPartial('display','<span>{{this.prompt}}:</span> <span>{{this.value}}</span>');
-    Handlebars.registerHelper('find_id', function(data) {
+(function(h){
+    var x=5;
+    h.registerPartial('link','<a class="{{rel}}" href="{{href}}{{>query_param}}">{{prompt}}</a>');
+    h.registerPartial('query_param','{{#each data}}{{#if @first}}?{{/if}}{{name}}={{value}}{{#unless @last}}&{{/unless}}{{/each}}');
+    h.registerPartial('input','<label for="{{name}}">{{prompt}}</label><input name="{{name}}" value="{{value}}"/>');
+    h.registerPartial('display','<span>{{this.prompt}}:</span> <span>{{this.value}}</span>');
+    h.registerHelper('find_id', function(data) {
       for (var i = 0; i < length; i++) {
-        item_key = data[i];
-        if (item_key.name==='id') 
-            return item_key.value;
+        if (isId(data[i])) 
+            return data[i].value;
       }
-    });  
+    });
+})(Handlebars)
+
+$(document).ready(function() {
+ 
     link_template = Handlebars.compile($("#link-template").html());
     item_template = Handlebars.compile($("#item-template").html());
     query_template = Handlebars.compile($("#query-template").html());
     template_template = Handlebars.compile($("#template-template").html());
+
+
     httpRequest('GET',url);
  
 });
-var putForm=function(e){
-    var form=e.currentTarget.closest('form');
-    var arrayData=$(form).serializeArray()
-    var formData={"template":{"data":arrayData}}
-    httpRequest('PUT',form.action,formData);
 
-};
-var postForm=function(e){
-    var form=e.currentTarget.closest('form');
-    var arrayData=$(form).serializeArray()
-    var formData={"template":{"data":arrayData}}
-    httpRequest('POST',form.action,formData);
 
-};
-var httpRequest=function(method, url, form){
+var httpRequest=function(method, href, form){
       $.ajax({
-        url: url,
+        url: href || url,
         data:JSON.stringify(form),
         contentType: "application/vnd.collection+json",
         type:method || "GET",
@@ -60,30 +54,43 @@ var httpRequest=function(method, url, form){
         $("#items").html(data.collection.items.map(item_template));
         $("#queries").html(data.collection.queries.map(query_template));
         $("#templates").html(template_template(data.collection.template));
+        
         $("#items form").hide();
-        $("#items button[type='submit']").bind('click',function(e){
+
+        $("form button[type='submit']").bind('click',function(e){
             e.preventDefault();
-            putForm(e);
+            handleForm(e);
         });
-        $("#template button[type='submit']").bind('click',function(e){
-            e.preventDefault();
-            postForm(e);
-        });
+        
         $("a").bind('click',function(e){
             e.preventDefault();
-            bindLinks(e);
+            handleLinks(e);
         });
-        $(".create-form form").attr("action",url);
+        
+  
     });    
 };
-var bindLinks=function(e){
+
+var getFormData=function(form){
+    var arrayData=form.serializeArray();
+    return {"template":{"data":arrayData}}
+};
+
+var handleForm=function(e){
+    var form=$(e.currentTarget.closest('form'));
+    var href=form.attr('action');
+    var method=form.attr('method');
+    httpRequest(method,href,getFormData(form));
+
+};
+
+var handleLinks=function(e){
     var el=e.currentTarget;
     if(el.closest("div.item")){
         var parentId=el.closest("div.item").id
         if(el.className==='edit'){
             $("#" + parentId + " ul").hide();
             $("#" + parentId + " form").show();
-            $("#" + parentId + " form").attr("method","POST");
             return;
         };
         if(el.className==='cancel_edit'){
